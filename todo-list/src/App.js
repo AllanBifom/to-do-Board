f18231c4dc2ee97654ebdec5db2ff8197ac47ca2
Allan Bifom (aeb672) <aeb672@usask.ca>
2025-01-06 00:56:57 -0600

    Finished functional development for Input component, adding styling now

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import Input from './components/Input';
+import Board from './components/Board';
+import React, {useState} from 'react';
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
   return (
-    <>
-    <h1>
+    <div className='flex flex-col items-center justify-center py-8 gap-4'>
+    <h1 className='text-xl font-semibold'>
       TO-DO Board List
     </h1>
-    </>
+    <Input taskList={taskList} setTaskList={setTaskList} />
+    <Board />
+    </div>
   );
 }
 
--- todo-list/src/components/Input.jsx
+++ todo-list/src/components/Input.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Input() {
+function Input({taskList,setTaskList}) {
+    const [input, setInput] = useState("");
+    const addTask = (e) => {
+        e.preventDefault()
+        setTaskList([...taskList, input]);
+        setInput("");
+    }
   return (
-    <div>Input</div>
+    <div>
+        <form className='flex flex-row items-center gap-3'>
+            <input
+            className="border rounded-lg py-1.5 px-2.5 text-lg"
+            type='text'
+            placeholder='Add a task'
+            value={input}
+            onChange={(e)=>setInput(e.target.value)}
+            />
+
+            <button
+            onClick={addTask}
+            className='bg-violet-400 text-white py-2 px-4 rounded'
+            >
+                Add
+            </button>
+        </form>
+    </div>
   )
 }
 
